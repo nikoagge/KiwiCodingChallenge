c93fcfa08c48612b7bade87641de3a3489e93a60
nikoagge <nikoagge@gmail.com>
2022-06-18 18:47:49 +0300

    Initial Commit

---
+++ KiwiCodingChallenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KiwiCodingChallenge
+//
+//  Created by Nikos Aggelidis on 18/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
